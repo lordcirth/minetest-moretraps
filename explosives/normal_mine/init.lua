a27fe7a1ed973c64a3a00059b4100a9464bfa0e9
PyroCop <jacob.brunsting@gmail.com>
2013-06-14 10:08:58 -0300

    Update init.lua

--- explosives/normal_mine/init.lua
+++ explosives/normal_mine/init.lua
@@ -2,41 +2,51 @@
 
 local countdown_time = 10
 
---register entities & nodes
+
 minetest.register_craftitem("normal_mine:gunpowder", {
     inventory_image = "throwing_string.png",
 })
+
 minetest.register_node("normal_mine:mine", {
     description  = "Mine",
-    tiles = {"inactive_top.png", "inactive_bottom.png",
-        "inactive_side.png", "inactive_side.png",
-        "inactive_side.png", "inactive_side.png",
+    drawtype = "nodebox",
+    inventory_image  = "inventory_image.png",
+    tiles = {
+    	"inactive_top.png", 
+    	"inactive_bottom.png",
+        "inactive_side.png", 
+        "inactive_side.png",
+        "inactive_side.png", 
+        "inactive_side.png",
     },
-    on_punch = function (pos, node)                     --on punch, make counting down true, which causes the inactive mine abm to begin counting down to mine activation
+    paramtype = "light",
+    groups = {cracky=2,oddly_breakable_by_hand=5,flammable=3,explody=9},
+    node_box = {
+        type = "fixed",
+        fixed = {
+        	{-.25,-.5,-.25,.25,-.375,.25},
+        }
+    },
+    on_punch = function (pos, node) 
         local meta = minetest.env:get_meta(pos)
         meta:set_int("Counting_Down", 1)
         meta:set_int("Time_Until_Activation", countdown_time)
     end,
-    paramtype = "light",
-    inventory_image  = "inventory_image.png",
-    groups = {cracky=2,oddly_breakable_by_hand=5,flammable=3,explody=9},
-    drawtype = "nodebox",
-    node_box = {
-        type = "fixed",
-        fixed = {
-        {-.25,-.5,-.25,.25,-.375,.25},
-        }
-    },
 })
+
 minetest.register_node("normal_mine:active_mine", {
-    tiles = {"active_top.png", "active_bottom.png",
-        "active_side.png", "active_side.png",
-        "active_side.png", "active_side.png",
+    drawtype = "nodebox",
+	inventory_image  = "mines_remote_inactive.png",
+    tiles = {
+    	"active_top.png", 
+    	"active_bottom.png",
+        "active_side.png", 
+        "active_side.png",
+        "active_side.png", 
+        "active_side.png",
     },
     paramtype = "light",
-    inventory_image  = "mines_remote_inactive.png",
     groups = {cracky=2,oddly_breakable_by_hand=5,flammable=3,explody=9},
-    drawtype = "nodebox",
     node_box = {
         type = "fixed",
         fixed = {
@@ -44,42 +54,37 @@ minetest.register_node("normal_mine:active_mine", {
         }
     },
 })
---ABM's
+
 minetest.register_abm({
     nodenames = {"normal_mine:active_mine"},
     interval = 1,
     chance = 1,
     action = function(pos)
-            local objs = minetest.env:get_objects_inside_radius({x=pos.x,y=pos.y,z=pos.z}, 5)         
-                                       
-local node_name = "normal_mine:active_mine"
-	local self = not_an_entity
+		local objs = minetest.env:get_objects_inside_radius({x=pos.x,y=pos.y,z=pos.z}, 5)                                            
+		local node_name = "normal_mine:active_mine"
+		local self = not_an_entity
 		explode(pos, node_name, self)
-
-        end,
+	end,
 })
+
 minetest.register_abm({
     nodenames = {"normal_mine:mine"},
     interval = 1,
     chance = 1,
     action = function(pos)
         local meta = minetest.env:get_meta(pos)
-       
         if meta:get_int("Counting_Down") == 1 then
             if meta:get_int("Time_Until_Activation")>0 then
-
                 meta:set_int("Time_Until_Activation", meta:get_int("Time_Until_Activation")-1)
             else
                 local node = minetest.env:get_node(pos)
                 node.name = ("normal_mine:active_mine")
-                print ("placing active mine node")
                 minetest.env:add_node(pos,node)
             end
         end
     end,
 })
 
---crafting recipies
 minetest.register_craft({
     output = '"normal_mine:mine" 4',
     recipe = {
@@ -88,6 +93,7 @@ minetest.register_craft({
         {'', 'default:steel_ingot', ''},
     }
 })
+
 minetest.register_craft({
     output = '"normal_mine:gunpowder" 4',
     recipe = {
